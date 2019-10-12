662ebe7b60d0c6461fe245f3ff6e38616824ae17
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-11 22:00:28 -0700

    [FINISHED] titanic dataset with pyspark

--- Classwork/hw_3_pyspark/titanic_pyspark.py
+++ Classwork/hw_3_pyspark/titanic_pyspark.py
@@ -6,25 +6,6 @@ from pyspark import SparkContext
 sc = SparkContext()
 
 
-# """
-# VARIABLE DESCRIPTIONS:
-# survival        Survival
-#                 (0 = No; 1 = Yes)
-# pclass          Passenger Class
-#                 (1 = 1st; 2 = 2nd; 3 = 3rd)
-# name            Name
-# sex             Sex
-# age             Age
-# sibsp           Number of Siblings/Spouses Aboard
-# parch           Number of Parents/Children Aboard
-# ticket          Ticket Number
-# fare            Passenger Fare
-# cabin           Cabin
-# embarked        Port of Embarkation
-#                 (C = Cherbourg; Q = Queenstown; S = Southampton)
-# """
-
-
 spark = SparkSession \
     .builder \
     .appName("Python Spark regression example") \
